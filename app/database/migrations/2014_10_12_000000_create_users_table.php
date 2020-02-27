e245ce9e68c4c9ed107cbe0559417e3e7974fd14
Muhammad Fakhri Putra Supriyadi <fakhriputra123s@gmail.com>
2020-02-27 17:21:07 +0700

    fix user api_token migration

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,6 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('api_token', 80)
-                    ->after('password')
                     ->unique()
                     ->nullable()
                     ->default(null);
